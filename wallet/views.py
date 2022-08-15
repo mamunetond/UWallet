90c120c1b5eb7ab5a43a9f63a82b982cf2bf8b24
mamunetond <47578828+mamunetond@users.noreply.github.com>
2022-08-15 14:44:59 -0500

    Se agraron las funciones de income para los ingresos y cost para los gastos

--- wallet/views.py
+++ wallet/views.py
@@ -7,3 +7,9 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to home page <h1>')
     return render(request,'home.html', {'name': 'Greg Lim'})
+
+def income(request):
+    return render(request, 'income.html', {'name': 'Greg Lim'})
+
+def cost(request):
+    return render(request, 'cost.html', {'name': 'Greg Lim'})
