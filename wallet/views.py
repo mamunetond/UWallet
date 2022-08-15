a5d7ce87640d229762e460c100ec86f795a18c8d
mamunetond <47578828+mamunetond@users.noreply.github.com>
2022-08-15 13:34:50 -0500

    Configuración del Framework Django con su arquitectura Modelo Vista Templates (MVT)

---
+++ wallet/views.py
@@ -0,0 +1,9 @@
+from urllib import request
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    #return HttpResponse('<h1>Welcome to home page <h1>')
+    return render(request,'home.html', {'name': 'Greg Lim'})
