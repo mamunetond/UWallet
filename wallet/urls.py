c06f11ee7daefefb849c5a923e61f6b0f0bd84d4
Camila83 <cmejiam10@eafit.edu.co>
2022-09-05 19:31:21 -0500

    Parte de Spents, agrega y se muestra en lista

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('income/', views.income_view, name = 'register_income'),
-    path('registerincomes/', views.registerincomes)
+    path('registerincomes/', views.registerincomes),
+    path('spent/', views.spent_view, name = 'register_spent'),
+    path('registerspents/', views.registerspents)
+    
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -2,15 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .templates.income.Income import RegisterIncome
 from .models import Income
+from .models import Spent
 def index(request):
     return HttpResponse('HOME PAGE')
 
 def income_view(request):
     incomes = Income.objects.all()
     return render(request, 'income/register_income.html', {'incomeslist': incomes})
-    #register_income = RegisterIncome()
-    #return render(request, 'income/register_income.html', {'register_income': register_income})
     
+
+
 def registerincomes(request):
     value_income= request.POST['intvalue_income']
     type_income= request.POST['txttype_income']
@@ -19,3 +20,15 @@ def registerincomes(request):
     
     income = Income.objects.create(value_income= value_income, type_income= type_income, detail_income= detail_income, date_income= date_income)
     return redirect('/income/')
+
+def spent_view(request):
+    spents = Spent.objects.all()
+    return render(request, 'spent/register_spent.html', {'spentslist': spents})
+
+def registerspents(request):
+    value_spent= request.POST['intvalue_spent']
+    detail_spent= request.POST['txtdetail_spent']
+    date_spent=request.POST['datedate_spent']
+    
+    spent = Spent.objects.create(value_spent= value_spent, detail_spent= detail_spent, date_spent= date_spent)
+    return redirect('/spent/')
